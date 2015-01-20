8fb2ecc244fb0cce0c681d7741bb40bb9c6fe8e8
elison22 <behatdude@gmail.com>
2015-01-19 23:59:25 -0700

    Finished Java Docs for Board, HexTile, and Edge

--- java/src/model/board/Board.java
+++ java/src/model/board/Board.java
@@ -5,27 +5,41 @@ import model.board.mapunits.HexTile;
 import model.board.mapunits.Vertex;
 import shared.definitions.HexType;
 import shared.locations.HexLocation;
-import shared.locations.VertexLocation;
 
 import java.util.ArrayList;
 import java.util.Random;
 
 /**
  * Created by brandt on 1/17/15.
+ * <p>
+ * This class holds information about all the elements of a Settlers of Catan board. This
+ * includes collections of HexTile objects, Vertex objects, and Edge objects. These are the
+ * actual objects, not references to objects based on some relative location. The board
+ * objects also remembers the HexLocation for the robber.
+ * </p>
+ * <p>
+ * The HexTile objects are organized by a slightly adjusted x/y coordinate system, which
+ * has values in the range -2 to 2.
+ * </p>
  */
 public class Board {
 
+    /** The HexTile objects that make up this Board */
     private ArrayList<HexTile> tiles = new ArrayList<HexTile>() {};
+    /** The Vertex objects that make up this Board */
     private ArrayList<Vertex> vertices = new ArrayList<Vertex>() {};
+    /** The Edge objects that make up this Board */
     private ArrayList<Edge> edges = new ArrayList<Edge>() {};
+    /** The HexLocation that represents the robber */
     private HexLocation robber;
 
     /**
      * Creates a new Board object with HexTile objects for the whole board including
      * diceNums, and actual HexLocations. It also initializes the robbers location to
      * the Desert, which has a diceNum of 0.
+     * @throws BoardException Thrown only when there's a problem with the code that initializes this object.
      */
-    public Board() {
+    public Board() throws BoardException {
 
         Random random = new Random();
         int nextRand;
@@ -57,10 +71,39 @@ public class Board {
         }
     }
 
-    private int setRobber(int temp, int x, int y){
-        robber = new HexLocation(x, y);
-        return temp;
+    /**
+     * This is called in the constructor to simultaneously set the diceNum of a
+     * HexTile and assign the robber to that same HexTile.
+     * @param diceNum The diceNum for the HexTile to receive the robber.
+     * @param x The x location for the robber.
+     * @param y The y location for the robber.
+     * @return The diceNum for the HexTile to receive the robber.
+     * @throws BoardException Thrown when the x or y values are outside the range -2 to 2
+     */
+    private int setRobber(int diceNum, int x, int y) throws BoardException{
+        setRobber(x, y);
+        return diceNum;
     }
 
+    /**
+     * Set the robber's new location using x/y coordinates
+     * @param x The robber's new x location
+     * @param y The robber's new y location
+     * @throws BoardException Thrown when the x or y values are outside the range -2 to 2
+     */
+    public void setRobber(int x, int y) throws BoardException{
+        //TODO: implement the check to make sure the robber is actually on the board
+        robber = new HexLocation(x, y);
+    }
+
+    /**
+     * Set the robber's new location using a HexLocation object
+     * @param newLocation The HexLocation for the robber's new location
+     * @throws BoardException Thrown when the x or y values are outside the range -2 to 2
+     */
+    public void setRobber(HexLocation newLocation) throws BoardException{
+        //TODO: implement the check to make sure the robber is actually on the board
+        robber = newLocation;
+    }
 
 }
--- java/src/model/board/mapunits/HexTile.java
+++ java/src/model/board/mapunits/HexTile.java
@@ -1,38 +1,62 @@
 package model.board.mapunits;
 
+import model.board.BoardException;
 import shared.definitions.HexType;
 import shared.locations.HexLocation;
-import shared.locations.VertexLocation;
+import shared.locations.VertexDirection;
 
 import java.util.TreeMap;
 
 /**
  * Created by brandt on 1/17/15.
+ * <p>
  * Contains all the information related to an individual hex tile
  * on a Catan board. This includes the tile's type, location, dice roll token value,
  * and a map pairing each of its vertex locations to the actual vertex object they refer to.
- *
+ * </p>
  * This object is primarily used for storage, so its methods are mostly getters and setters.
  */
 public class HexTile {
 
+    /** The value of the dice roll token for this HexTile. */
     private int diceNum;
+    /** The resource type for this HexTile. */
     private HexType type;
+    /** The location for this HexTile. */
     private HexLocation location;
-    private TreeMap<VertexLocation, Vertex> vertices;
+    /** A map that pairs this HexTile's relative vertex location to the actual Vertex object*/
+    private TreeMap<VertexDirection, Vertex> vertices;
 
+    /**
+     * Constructor to initialize the field type.
+     * @param type The HexType for this HexTile.
+     */
     public HexTile(HexType type) {
         this.type = type;
-        vertices = new TreeMap<VertexLocation, Vertex>();
+        vertices = new TreeMap<VertexDirection, Vertex>();
     }
 
+    /**
+     * Constructor to initialize the fields type and location.
+     * @param type The HexType for this HexTile.
+     * @param location The HexLocation for this HexTile.
+     */
     public HexTile(HexType type, HexLocation location) {
         this(type);
         this.location = location;
     }
 
-    public HexTile(HexType type, int diceNum, HexLocation location) {
+    /**
+     * Constructor to initialize the fields type, location, and diceNum.
+     * @param type The HexType for this HexTile.
+     * @param diceNum An integer 0-12 indicating the value of the dice roll token. diceNum may
+     *                be set to zero only in the case of HexType as Desert.
+     * @param location The HexLocation for this HexTile.
+     * @throws BoardException Thrown when diceNum is outside the range 0-12.
+     */
+    public HexTile(HexType type, int diceNum, HexLocation location) throws BoardException{
         this(type,location);
+        if(diceNum < 0 || diceNum > 12) throw new BoardException("Cannot set diceNum less than 0 or greater than 12 in HexTile constructor");
         this.diceNum = diceNum;
     }
 
@@ -40,6 +64,11 @@ public class HexTile {
         return diceNum;
     }
 
+    /**
+     * Setter for the diceNum field, which checks to make sure the diceNum param is valid.
+     * @param diceNum An integer 2-12 inclusive.
+     * @return True unless diceNum is outside the range 2-12.
+     */
     public boolean setDiceNum(int diceNum) {
         if(diceNum < 2 || diceNum > 12) return false;
         this.diceNum = diceNum;
@@ -54,7 +83,12 @@ public class HexTile {
         this.type = type;
     }
 
-    public Vertex getVertex(VertexLocation vLoc) {
+    /**
+     * Obtains a Vertex object using the VertexDirection relative to this HexTile.
+     * @param vLoc The location of a vertex relative to this HexTile.
+     * @return The actual Vertex object indicated by the vLoc parameter.
+     */
+    public Vertex getVertex(VertexDirection vLoc) {
         return vertices.get(vLoc);
     }
 
