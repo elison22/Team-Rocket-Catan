b88f79a06c6f2ad261f48ca8b885fc555d88d227
Hayden Newey <hayden.newey@gmail.com>
2015-01-21 12:11:15 -0700

    Player and PlayerManager Javadocs

--- java/src/model/player/Player.java
+++ java/src/model/player/Player.java
@@ -4,10 +4,36 @@ import model.cards.PlayerBank;
 
 /**
  * Created by brandt on 1/17/15.
+ * <p>
+ * This class is used to hold all of the data for a specific player in the game. That
+ * data includes the player's hand of cards called a PlayerBank, their name, the number
+ * of victory points they have earned, and their index in the list of players. This index
+ * functions as an identifier for each player.
+ * </p>
  */
 public class Player {
 
-    PlayerBank bank;
-    //other stuff
+    /** The player's index in the list of players */
+    private playerIdx;
+    /** The player's bank of cards */
+    private bank;
+    /** The player's name */
+    private playerName;
+    /** The player's victory points */
+    private victoryPoints;
+
+    /**
+     * Creates a new Player object with an empty PlayerBank object and zero victory points
+     * @param playerIdx Initializes the player's index in the list of players
+     * @param name The name the user entered upon starting the game.
+     */
+    public Player(playerIdx, playerName){
+        this.playerName = playerName;
+        this.victoryPoints = 0;
+        this.bank = new PlayerBank();
+        this.playerIdx = playerIdx;
+    }
+
+
 
 }
--- java/src/model/player/PlayerManager.java
+++ java/src/model/player/PlayerManager.java
@@ -4,13 +4,43 @@ import java.util.ArrayList;
 
 /**
  * Created by brandt on 1/17/15.
+ * <p>
+ * This class holds a collection of Player objects and contains the methods necessary to
+ * create and remove players from a game. The Model Facade will also communicate with
+ * this class to determine if a player can be added or not
+ * </p>
  */
 public class PlayerManager {
 
+    /**The List of Player objects in the game. The number of players can range from 0-4*/
     ArrayList<Player> players;
 
-    public boolean createPlayer(){
+    /**
+     * Method called by Facade to determine if a player can be added to the current game
+     */
+    public boolean canAddPlayer(){
+        return true;
+    }
+
+    /**
+     * Creates a new Player object and adds it to the List of current players.
+     * @return Whether the Player object was successfully created or not
+     * @throws createPlayerException Thrown if there is insufficient data to create a new Player. Also
+     * acts as a second check to make sure no more than four players are in a game at a time.
+     */
+    public boolean createPlayer() throws createPlayerException{
         //add a player to the list
+        return true;
+    }
+
+    /**
+     * Removes a Player object from the List of current players.
+     * @param The index of the player to be removed
+     * @return Whether the specified player was removed or not.
+     * @throws removePlayerException Thrown if there is a client-side error in removing the player
+     */
+    public boolean removePlayer(Player.playerIdx) throws removePlayerException{
+        //remove a player from the list
         return true;
     }
 
