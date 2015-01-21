11b4d4a3860929327f2a11d841b43489be8077e9
Hayden Newey <hayden.newey@gmail.com>
2015-01-21 15:49:38 -0700

    update to Javadocs

--- java/src/model/cards/DevCard.java
+++ java/src/model/cards/DevCard.java
@@ -4,4 +4,17 @@ package model.cards;
  * Created by brandt on 1/17/15.
  */
 public class DevCard {
+
+    private String type;
+    /**
+     * Creates a DevCard object
+     * @param type The type of development card to create
+     */
+    public DevCard(String type){
+        this.type = type;
+    }
+
+    public setEffect(String type){
+        //a switch statement probably?
+    }
 }
--- java/src/model/cards/PlayerBank.java
+++ java/src/model/cards/PlayerBank.java
@@ -2,21 +2,38 @@ package model.cards;
 
 /**
  * Created by brandt on 1/17/15.
+ * Extension of the CardBank class that holds all of the cards
+ * each player has in their hand. Upon creation, the PlayerBank will
+ * be empty.
  */
 public class PlayerBank extends CardBank{
 
+    /**
+     * Creates a PlayerBank object and initializes the player's
+     * held resources to zero
+     */
     public PlayerBank(){
         //initialize everything to 0
     }
 
+    /**
+     * Used to take a development card from the GameBank and add it
+     * to the player's list of development cards
+     * @return
+     */
     public boolean takeDevCard(){
         //take from the bank
         //add to card list
         return true;
     }
 
-    public boolean playDevCard(DevCard card){
+    /**
+     * Used by a player to play a development card
+     * @param card The type of development card to be played
+     */
+    public void playDevCard(DevCard card){
         //maybe some inheritance or an interface?
+        card.effect();
         return true;
     }
 
