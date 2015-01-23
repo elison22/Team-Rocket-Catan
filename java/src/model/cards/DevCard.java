18080adf104d2517619f114173cf06ac6df0aabe
elison22 <behatdude@gmail.com>
2015-01-22 22:39:16 -0700

    This also had a build error. I fixed that so we can generate the JavaDoc.

--- java/src/model/cards/DevCard.java
+++ java/src/model/cards/DevCard.java
@@ -1,20 +1,22 @@
 package model.cards;
 
+import shared.definitions.DevCardType;
+
 /**
  * Created by brandt on 1/17/15.
  */
 public class DevCard {
 
-    private String type;
+    private DevCardType type;
     /**
      * Creates a DevCard object
      * @param type The type of development card to create
      */
-    public DevCard(String type){
+    public DevCard(DevCardType type){
         this.type = type;
     }
 
-    public setEffect(String type){
+    public void setEffect(DevCardType type){
         //a switch statement probably?
     }
 }
