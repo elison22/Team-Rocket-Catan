d78be2c145fccebfbbe1668f21cea08bafe7ac96
Andrew <wally451@sbcglobal.net>
2015-02-01 13:16:42 -0700

    added attributes and getters

--- java/src/model/trade/MaritimeTrade.java
+++ java/src/model/trade/MaritimeTrade.java
@@ -3,6 +3,11 @@ package model.trade;
 import model.cards.ResourceSet;
 
 public class MaritimeTrade {
+	
+	private int sender;
+	private int receiver;
+	private ResourceSet offer;
+	private String tradeType;
 
     public void setSender(int sender) {
         // TODO Auto-generated method stub
@@ -18,5 +23,17 @@ public class MaritimeTrade {
         // TODO Auto-generated method stub
 
     }
+    
+    public int getSender() {
+		return sender;
+	}
+
+	public ResourceSet getOffer() {
+		return offer;
+	}
+
+	public String getTradeType() {
+		return tradeType;
+	}
 
 }
