3fc609db6abcb07dee80c4fafd5c3c237369e267
Hayden Newey <hayden.newey@gmail.com>
2015-02-21 15:48:28 -0700

    Fixed update method

--- java/src/client/map/MapController.java
+++ java/src/client/map/MapController.java
@@ -201,7 +201,7 @@ public class MapController extends Controller implements IMapController, Observe
 	@Override
 	public void update(Observable o, Object arg) {
 		// TODO Auto-generated method stub
-		
+		initFromModel();
 	}
 	
 }
