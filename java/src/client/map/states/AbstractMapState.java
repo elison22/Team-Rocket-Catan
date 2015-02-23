276c13ccb698792ccab1fdd3684f437762ffcb1d
Hayden Newey <hayden.newey@gmail.com>
2015-02-23 01:35:36 -0700

    Finished creating abstract methods

--- java/src/client/map/states/AbstractMapState.java
+++ java/src/client/map/states/AbstractMapState.java
@@ -1,7 +1,12 @@
 package client.map.states;
 
+import client.data.RobPlayerInfo;
 import client.map.IMapController;
 import client.map.MapController;
+import shared.definitions.PieceType;
+import shared.locations.EdgeLocation;
+import shared.locations.HexLocation;
+import shared.locations.VertexLocation;
 
 /**
  * Created by brandt on 2/18/15.
@@ -11,5 +16,32 @@ public abstract class AbstractMapState {
     public abstract void update();
 
     public abstract void start(MapController controller);
+
+    public abstract boolean canBuildRoad(EdgeLocation edgeLoc);
+
+    public abstract boolean canBuildSettlement(VertexLocation vertLoc);
+
+    public abstract boolean canBuildCity(VertexLocation vertLoc);
+
+    public abstract boolean canPlaceRobber(HexLocation hexLoc);
+
+    public abstract void placeRoad(EdgeLocation edgeLoc);
+
+    public abstract void placeSettlement(VertexLocation vertLoc);
+
+    public abstract void placeCity(VertexLocation vertLoc);
+
+    public abstract void placeRobber(HexLocation hexLoc);
+
+    public abstract void startMove(PieceType pieceType);
+
+    public abstract void cancelMove();
+
+    public abstract void playSoldierCard();
+
+    public abstract void playRoadBuildingCard();
+
+    public abstract void robPlayer(RobPlayerInfo victim);
+
 
 }
