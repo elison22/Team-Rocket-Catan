bdbe3874eff029edb1ab39b61317e680b3d2ff83
Andrew <wally451@sbcglobal.net>
2015-02-28 02:56:30 -0700

    new class

---
+++ java/src/model/trade/TradeOffer.java
@@ -0,0 +1,39 @@
+package model.trade;
+
+import java.util.HashMap;
+
+import serializer.json.JsonResourceList;
+import serializer.json.JsonTradeOffer;
+import shared.definitions.ResourceType;
+
+public class TradeOffer {
+
+	private int sender;
+	private int receiver;
+	private HashMap<ResourceType, Integer> resources;
+	
+	public TradeOffer(JsonTradeOffer trade) {
+		sender = trade.getSender();
+		receiver = trade.getReceiver();
+		JsonResourceList list = trade.getOffer();
+		
+		resources = new HashMap<ResourceType, Integer>();
+		resources.put(ResourceType.BRICK, list.getBrick());
+		resources.put(ResourceType.ORE, list.getOre());
+		resources.put(ResourceType.SHEEP, list.getSheep());
+		resources.put(ResourceType.WHEAT, list.getWheat());
+		resources.put(ResourceType.WOOD, list.getWood());
+	}
+
+	public int getSender() {
+		return sender;
+	}
+
+	public int getReceiver() {
+		return receiver;
+	}
+
+	public HashMap<ResourceType, Integer> getResources() {
+		return resources;
+	}
+}
