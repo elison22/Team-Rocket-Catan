36a18af3fb13d11bd7db28c1d9af5cdd52f2a89d
Andrew <wally451@sbcglobal.net>
2015-02-28 00:39:09 -0700

    mostly implemented domestic trade.. still not sure how to know when you are being offered a trade

--- java/src/model/trade/DomesticTrade.java
+++ java/src/model/trade/DomesticTrade.java
@@ -1,28 +1,38 @@
 package model.trade;
 
+import java.util.HashMap;
+
+import shared.definitions.ResourceType;
 import model.cards.ResourceSet;
 
 public class DomesticTrade {
 
 	private int sender;
 	private int receiver;
-	private ResourceSet offer;
+	//private ResourceSet offer;
+	private HashMap<ResourceType,Integer> offer;
 	
-	public DomesticTrade(int sender, int receiver, ResourceSet offer) {
+	/*public DomesticTrade(int sender, int receiver, ResourceSet offer) {
 		this.sender = sender;
 		this. receiver = receiver;
 		this.offer = offer;
+	}*/
+	
+	public DomesticTrade(int sender) {
+		this.sender = sender;
+		receiver = -1;
+		offer = null;
 	}
 	
-    public void setSender(int sender) {
+    /*public void setSender(int sender) {
         this.sender = sender;
-    }
+    }*/
 
     public void setReceiver(int receiver) {
         this.receiver = receiver;
     }
 
-    public void setOffer(ResourceSet offer) {
+    public void setOffer(HashMap<ResourceType,Integer> offer) {
         this.offer = offer;
     }
 
@@ -34,7 +44,7 @@ public class DomesticTrade {
 		return receiver;
 	}
 
-	public ResourceSet getOffer() {
+	public HashMap<ResourceType,Integer> getOffer() {
 		return offer;
 	}
 
