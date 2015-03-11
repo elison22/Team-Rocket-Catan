432a55b83eb532ea6aab435fd1977f8a5d4465e6
Hayden Newey <hayden.newey@gmail.com>
2015-03-11 02:42:08 -0600

    created empty methods in ModelFacade class

---
+++ java/server/facade/ModelFacade.java
@@ -0,0 +1,153 @@
+package facade;
+
+
+/**
+ *
+ */
+public class ModelFacade {
+
+    public String listGames(){
+
+        return null;
+    }
+
+    public String createGame(boolean randomTiles, boolean randomNumbers, boolean randomPorts, String name){
+
+        return null;
+    }
+
+    public String joinGame(int gameID, String color){
+
+        return null;
+    }
+
+    public String saveGame(int gameID, String fileName){
+
+        return null;
+    }
+
+    public String loadGame(String fileName){
+
+        return null;
+    }
+
+    public String getGameModel(int gameID){
+
+        return null;
+    }
+
+    public String resetGame(int gameID){
+
+        return null;
+    }
+
+    public String getGameCommands(int gameID){
+
+        return null;
+    }
+
+    public String executeGameCommands(int gameID, String gameCommands){
+
+        return null;
+    }
+
+    public String addAI(String AIType){
+
+        return null;
+    }
+
+    public String listAI(){
+
+        return null;
+    }
+
+    public String sendChat(){
+
+        return null;
+    }
+
+    public String rollNumber(){
+
+        return null;
+    }
+
+    public String robPlayer(){
+
+        return null;
+    }
+
+    public String finishTurn(){
+
+        return null;
+    }
+
+    public String buyDevCard(){
+
+        return null;
+    }
+
+    public String doYearOfPlenty(){
+
+        return null;
+    }
+
+    public String doRoadBuilding(){
+
+        return null;
+    }
+
+    public String doSoldier(){
+
+        return null;
+    }
+
+    public String doMonopoly(){
+
+        return null;
+    }
+
+    public String doMonument(){
+
+        return null;
+    }
+
+    public String buildRoad(){
+
+        return null;
+    }
+
+    public String buildSettlement(){
+
+        return null;
+    }
+
+    public String buildCity(){
+
+        return null;
+    }
+
+    public String offerTrade(){
+
+        return null;
+    }
+
+    public String acceptTrade(){
+
+        return null;
+    }
+
+    public String maritimeTrade(){
+
+        return null;
+    }
+
+    public String discardCards(){
+
+        return null;
+    }
+
+    public String changeLogLevel(){
+
+        return null;
+    }
+}
