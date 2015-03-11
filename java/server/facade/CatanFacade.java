7836da12e9425e0bb76a264ebbd6ca40e2f9782f
gseccles <gseccles@gmail.com>
2015-03-11 14:53:46 -0600

    started stubbing catan facade classes and subclasses

---
+++ java/server/facade/CatanFacade.java
@@ -0,0 +1,77 @@
+package server;
+
+import proxy.ServerException;
+
+public class CatanFacade {
+	public CatanFacade(){}
+
+	private GameManager gameManager;
+	
+	public String rollNumber(Object rollNumberParams) throws ServerException {
+		return null;
+	}
+	
+	public String robPlayer(Object robPlayerParams) throws ServerException {
+		return null;
+	}
+	
+	public String finishTurn(Object finishTurnParams) throws ServerException {
+		return null;
+	}
+	 
+	public String buyDevCard(Object buyDevCardParams) throws ServerException {
+		return null;
+	}
+	 
+	public String Year_of_Plenty(Object yearOfPlentyParams) throws ServerException {
+		return null;
+	}
+	 
+	public String Road_Building(Object roadBuildingParams) throws ServerException {
+		return null;
+	}
+	 
+	public String Soldier(Object soldierParams) throws ServerException {
+		return null;
+	}
+
+	public String Monopoly(Object monopolyParams) throws ServerException {
+		return null;
+	}
+	 
+	public String Monument(Object monumentParams) throws ServerException {
+		return null;
+	}
+	 
+	public String buildRoad(Object buildRoadParams) throws ServerException {
+		return null;
+	}
+	 
+	public String buildSettlement(Object buildSettlementParams) throws ServerException {
+		return null;
+	}
+	 
+	public String buildCity(Object buildCityParams) throws ServerException {
+		return null;
+	}
+	 
+	public String offerTrade(Object offerTradeParams) throws ServerException {
+		return null;
+	}
+	 
+	public String acceptTrade(Object acceptTradeParams) throws ServerException {
+		return null;
+	}
+	
+	public String maritimeTrade(Object maritimeTradeParams) throws ServerException {
+		return null;
+	}
+	
+	public String discardCards(Object discardCardsParams) throws ServerException {
+		return null;
+	}
+	
+	public boolean changeLogLevel(Object changeLogLevelParams) throws ServerException {
+		return false;
+	}
+}
---
+++ java/server/facade/GameManager.java
@@ -0,0 +1,8 @@
+package server;
+
+import java.util.ArrayList;
+
+public class GameManager {
+	
+	private ArrayList<CatanModel> games;
+}
