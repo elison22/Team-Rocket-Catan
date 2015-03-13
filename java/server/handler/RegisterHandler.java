5daf7b0f69372d8cedb65d5da338832b9cc2512b
Andrew <wally451@sbcglobal.net>
2015-03-13 13:50:09 -0600

    added new handlers for the moves

--- java/server/handler/RegisterHandler.java
+++ java/server/handler/RegisterHandler.java
@@ -2,6 +2,8 @@ package handler;
 
 import java.io.IOException;
 
+import user.IUserFacade;
+
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import command.ICommandObject;
@@ -9,7 +11,7 @@ import command.ICommandObject;
 public class RegisterHandler implements HttpHandler {
 	
 	private ICommandObject commandObject;
-	// private IUserFacade userFacade;
+	private IUserFacade userFacade;
 
 	@Override
 	public void handle(HttpExchange arg0) throws IOException {
