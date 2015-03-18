feeb0d818b866b5a40212f5b528e2e9ebd02f2e1
Chad Bacon <Chadsbacon@gmail.com>
2015-03-18 08:03:55 -0600

    Partly implemented Login and Register handlers. We just need to prepare and send back cookies with the response.

--- java/server/handler/LoginHandler.java
+++ java/server/handler/LoginHandler.java
@@ -1,26 +1,54 @@
 package handler;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 
+import shared.dto.Login_Params;
 import user.IUserFacade;
 
+import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-import command.ICommandObject;
 
 public class LoginHandler implements HttpHandler {
 	
 	private IUserFacade userFacade;
+	private Gson gson;
 	
 	public LoginHandler(IUserFacade userFacade) {
 		super();
 		this.userFacade = userFacade;
+		gson = new Gson();
 	}
 
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
-		// TODO Auto-generated method stub
-
+		// Prepare to read the RequestBody inputStream
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(exchange.getRequestBody(), "UTF-8"));
+		StringBuilder stringBuilder = new StringBuilder();
+		
+		// Process inputStream
+		String request;
+		while ((request = bufferedReader.readLine()) != null)
+			stringBuilder.append(request);
+		
+		// Create parameter object
+		Login_Params params = gson.fromJson(stringBuilder.toString(), Login_Params.class);
+		
+		if (userFacade.login(params)) {
+			// Login successful? HTTP_OK
+			exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
+			
+			//Prepare Cookies
+			
+			
+		} else {
+			// Login failed? HTTP_BAD_REQUEST
+			exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, 0);
+		}
+		
+		exchange.close();
 	}
-
 }
--- java/server/handler/RegisterHandler.java
+++ java/server/handler/RegisterHandler.java
@@ -2,38 +2,51 @@ package handler;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.URLDecoder;
 
+import shared.dto.Login_Params;
 import user.IUserFacade;
 
+import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 public class RegisterHandler implements HttpHandler {
 	
 	private IUserFacade userFacade;
+	private Gson gson;
 	
 	public RegisterHandler(IUserFacade userFacade) {
 		this.userFacade = userFacade;
+		this.gson = new Gson();
 	}
 
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
-
-		InputStream body = exchange.getRequestBody();
-		BufferedReader bw = new BufferedReader(new InputStreamReader(body));
-		String decoded = URLDecoder.decode(bw.readLine(), "UTF-8");
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(exchange.getRequestBody()));
+		StringBuilder stringBuilder = new StringBuilder();
 		
-		String[] params = decoded.split(",");
-		String name = params[0].substring(13, params[0].length() - 1);
-		String password = params[1].substring(12, params[1].length() - 2);
+		// Process inputStream
+		String request;
+		while ((request = bufferedReader.readLine()) != null)
+			stringBuilder.append(request);
 		
-		// do something with name/password
+		// Create parameter object
+		Login_Params params = gson.fromJson(stringBuilder.toString(), Login_Params.class);
 		
-		exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
+		if (userFacade.register(params)) {
+			// Login successful? HTTP_OK
+			exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
+			
+			//Prepare Cookies
+			
+			
+		} else {
+			// Login failed? HTTP_BAD_REQUEST
+			exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, 0);
+		}
+		
 		exchange.close();
 	}
 
