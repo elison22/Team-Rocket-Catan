076f37b4dda46d1f5f8c3c9da8c26267d3e0ba42
Chad Bacon <Chadsbacon@gmail.com>
2015-03-18 17:39:56 -0600

    Created a junit test for the usermanager.

---
+++ java/server/test/user/UserManagerTest.java
@@ -0,0 +1,39 @@
+package test.user;
+
+import static org.junit.Assert.*;
+
+import java.math.BigInteger;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import user.model.UserManager;
+
+public class UserManagerTest {
+	
+	UserManager userManager;
+
+	@Before
+	public void setUp() throws Exception {
+		userManager = new UserManager();
+	}
+
+	@Test
+	public void test() {
+		// Test that users are properly assigned unique ids
+		userManager.createNewUser("beef", "beefy");
+		userManager.createNewUser("beef1", "beefy");
+		userManager.createNewUser("beef2", "beefy");
+		
+		assertTrue(userManager.getUser("beef2").getId() == 2);
+		assertTrue(userManager.getUser("beef1").getId() == 1);
+		assertTrue(userManager.getUser("beef").getId() == 0);
+		
+		// Test that the userManager can handle large amounts of users
+		for (int i = 0; i <= 50000; ++i) {
+			userManager.createNewUser(new String("be" + i), "beefy");
+		}
+		assertTrue(userManager.getUser("be50000").getId() == 50003);
+	}
+
+}
