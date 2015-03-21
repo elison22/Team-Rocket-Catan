f5f2443bd9a9934e09cf898548f0147b5e069e12
Andrew <wally451@sbcglobal.net>
2015-03-20 18:54:36 -0600

    added more functionality to the joining game

--- java/server/handler/JoinGameHandler.java
+++ java/server/handler/JoinGameHandler.java
@@ -2,13 +2,16 @@ package handler;
 
 import java.io.IOException;
 
+import shared.dto.JoinGame_Params;
+
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import command.ICommandObject;
 import facade.IModelFacade;
 
-public class JoinGameHandler implements HttpHandler {
+public class JoinGameHandler extends NonMoveHandler {
 	
 	private IModelFacade modelFacade;
 	
@@ -19,9 +22,22 @@ public class JoinGameHandler implements HttpHandler {
 
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
-		// TODO Auto-generated method stub
+		String[] cookie = decodeCookie(exchange);
+		StringBuilder stringBuild = handleRequestBody(exchange);
+		JoinGame_Params params = gson.fromJson(stringBuild.toString(), JoinGame_Params.class);
 		
-		// set cookie header with game integer ID in response header
+		Headers head = exchange.getResponseHeaders();
+		head.set("Content-Type", "application/json");
+		
+		int gameId = modelFacade.getCreatedGameId();
+		String encode = "catan.game=" + gameId + ";Path=/;";
+		head.add("Set-cookie", encode);
+		
+		if(modelFacade.joinGame(params, cookie[1], new Integer(cookie[2]))) {
+			
+		} else {
+			
+		}
 	}
 
 }
