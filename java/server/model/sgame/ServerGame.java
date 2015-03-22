8eb2e355e72b6c095cfd9fa455d87b355ec62dc8
Andrew <wally451@sbcglobal.net>
2015-03-22 16:49:14 -0600

    fixed bug where game would auto win

--- java/server/model/sgame/ServerGame.java
+++ java/server/model/sgame/ServerGame.java
@@ -47,7 +47,7 @@ public class ServerGame {
     	aiList.add("LARGEST_ARMY");
         map = new ServerBoard(false, false, false);
         versionNumber = -1;
-    	//winner = -1;
+    	winner = -1;
     }
 
     public ServerGame(boolean randNumbers, boolean randTiles, boolean randPorts, String title) throws ServerBoardException {
@@ -59,6 +59,7 @@ public class ServerGame {
     	map = new ServerBoard(randNumbers, randTiles, randPorts);
     	chat = new ServerChat();
     	gameHistory = new ServerChat();
+    	winner = -1;
     }
 
     //**********************************************************
