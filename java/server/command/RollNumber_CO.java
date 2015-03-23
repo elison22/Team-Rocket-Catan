ac4bfe9bf96d548d58110ccda5fd366b00b3261c
Hayden Newey <hayden.newey@gmail.com>
2015-03-23 01:44:40 -0600

    implemented RollNumber command object

--- java/server/command/RollNumber_CO.java
+++ java/server/command/RollNumber_CO.java
@@ -1,5 +1,7 @@
 package command;
 
+import facade.GameManager;
+import model.sgame.ServerGame;
 import shared.dto.RollNumber_Params;
 import facade.IModelFacade;
 
@@ -13,19 +15,23 @@ public class RollNumber_CO implements ICommandObject {
 	
 	private int gameId;
 	private RollNumber_Params params;
+    private GameManager gameManager;
 
 	/**
 	 * @param gameId Id of the game where the dice roll is happening.
-	 * @param Parameters needed for rolling a number.
+	 * @param params needed for rolling a number.
 	 */
-	public RollNumber_CO(int gameId, RollNumber_Params params) {
+	public RollNumber_CO(int gameId, RollNumber_Params params, GameManager gameManager) {
 		this.gameId = gameId;
 		this.params = params;
+        this.gameManager = gameManager;
 	}
 
 	@Override
 	public boolean execute() {
-		return false;
+        ServerGame game = gameManager.getGame(gameId);
+        game.doRoll(params.getPlayerIndex(), params.getNumber());
+		return true;
 	}
 
 }
