1c93f2238b4b56f1e3adb7d053305009b932abf4
Chad Bacon <Chadsbacon@gmail.com>
2015-03-24 11:08:47 -0600

    Fleshed out the FinishTurnTest with more extensive tests. It should be pretty solid now

--- java/server/test/command/FinishTurnTest.java
+++ java/server/test/command/FinishTurnTest.java
@@ -28,32 +28,64 @@ public class FinishTurnTest {
         modelFacade.joinGame(new JoinGame_Params(0, "green"), "test2", 1);
         modelFacade.joinGame(new JoinGame_Params(0, "puce"), "test3", 2);
         modelFacade.joinGame(new JoinGame_Params(0, "blue"), "test4", 3);
-        modelFacade.buildRoad(0, new BuildRoad_Params(0, new EdgeLocation(new HexLocation(1, 1), EdgeDirection.North), true));
-        modelFacade.buildSettlement(0, new BuildSettlement_Params(0, new VertexLocation(new HexLocation(1, 1), VertexDirection.NorthEast), true));
-        modelFacade.buildRoad(0, new BuildRoad_Params(1, new EdgeLocation(new HexLocation(-2, -1), EdgeDirection.SouthEast), true));
-        modelFacade.buildSettlement(0, new BuildSettlement_Params(1, new VertexLocation(new HexLocation(-2, -1), VertexDirection.East), true));
-        modelFacade.buildRoad(0, new BuildRoad_Params(2, new EdgeLocation(new HexLocation(0, -2), EdgeDirection.NorthEast), true));
-        modelFacade.buildSettlement(0, new BuildSettlement_Params(2, new VertexLocation(new HexLocation(0, -2), VertexDirection.NorthEast), true));
-        modelFacade.buildRoad(0, new BuildRoad_Params(3, new EdgeLocation(new HexLocation(1, 1), EdgeDirection.South), true));
-        modelFacade.buildSettlement(0, new BuildSettlement_Params(3, new VertexLocation(new HexLocation(1, 1), VertexDirection.SouthWest), true));
-        modelFacade.buildRoad(0, new BuildRoad_Params(3, new EdgeLocation(new HexLocation(1, -1), EdgeDirection.North), true));
-        modelFacade.buildSettlement(0, new BuildSettlement_Params(3, new VertexLocation(new HexLocation(1, -1), VertexDirection.NorthEast), true));
-        modelFacade.buildRoad(0, new BuildRoad_Params(2, new EdgeLocation(new HexLocation(0, 0), EdgeDirection.NorthWest), true));
-        modelFacade.buildSettlement(0, new BuildSettlement_Params(2, new VertexLocation(new HexLocation(0, 0), VertexDirection.West), true));
-        modelFacade.buildRoad(0, new BuildRoad_Params(1, new EdgeLocation(new HexLocation(0, 1), EdgeDirection.NorthEast), true));
-        modelFacade.buildSettlement(0, new BuildSettlement_Params(1, new VertexLocation(new HexLocation(0, 1), VertexDirection.NorthEast), true));
-        modelFacade.buildRoad(0, new BuildRoad_Params(0, new EdgeLocation(new HexLocation(-1, 2), EdgeDirection.North), true));
-        modelFacade.buildSettlement(0, new BuildSettlement_Params(0, new VertexLocation(new HexLocation(-1, 2), VertexDirection.NorthWest), true));
-        modelFacade.rollNumber(0, new RollNumber_Params(0, 6));
+        
     }
 
     @Test
     public void test() {
-        // Test someone ending their turn when it's not their turn currently
-        assertNull(modelFacade.finishTurn(0, new FinishTurn_Params(2)));
-
+    	// Build player[0]'s initial road and settlement
+    	assertNotNull(modelFacade.buildRoad(0, new BuildRoad_Params(0, new EdgeLocation(new HexLocation(1, 1), EdgeDirection.North), true)));
+        assertNotNull(modelFacade.buildSettlement(0, new BuildSettlement_Params(0, new VertexLocation(new HexLocation(1, 1), VertexDirection.NorthEast), true)));
+        
+        // Check that it's player[1]'s turn
+        assertTrue(modelFacade.getGame(0).getTurnTracker().getCurrentPlayerIndex() == 1);
+        
+        // Build player[1]-[3]'s initial road and settlement
+        assertNotNull(modelFacade.buildRoad(0, new BuildRoad_Params(1, new EdgeLocation(new HexLocation(-2, -1), EdgeDirection.SouthEast), true)));
+        assertNotNull(modelFacade.buildSettlement(0, new BuildSettlement_Params(1, new VertexLocation(new HexLocation(-2, -1), VertexDirection.East), true)));
+        assertNotNull(modelFacade.buildRoad(0, new BuildRoad_Params(2, new EdgeLocation(new HexLocation(0, -2), EdgeDirection.NorthEast), true)));
+        assertNotNull(modelFacade.buildSettlement(0, new BuildSettlement_Params(2, new VertexLocation(new HexLocation(0, -2), VertexDirection.NorthEast), true)));
+        assertNotNull(modelFacade.buildRoad(0, new BuildRoad_Params(3, new EdgeLocation(new HexLocation(1, 1), EdgeDirection.South), true)));
+        assertNotNull(modelFacade.buildSettlement(0, new BuildSettlement_Params(3, new VertexLocation(new HexLocation(1, 1), VertexDirection.SouthWest), true)));
+        
+        // Check that it's still player[3]'s turn
+        assertTrue(modelFacade.getGame(0).getTurnTracker().getCurrentPlayerIndex() == 3);
+        
+        // Build player[3]'s second road and settlement
+        assertNotNull(modelFacade.buildRoad(0, new BuildRoad_Params(3, new EdgeLocation(new HexLocation(1, -1), EdgeDirection.North), true)));
+        assertNotNull(modelFacade.buildSettlement(0, new BuildSettlement_Params(3, new VertexLocation(new HexLocation(1, -1), VertexDirection.NorthEast), true)));
+        
+        // Check that it's player[2]'s turn
+        assertTrue(modelFacade.getGame(0).getTurnTracker().getCurrentPlayerIndex() == 2);
+        
+        // Build player[2]-[0]'s second road and settlement
+        assertNotNull(modelFacade.buildRoad(0, new BuildRoad_Params(2, new EdgeLocation(new HexLocation(0, 0), EdgeDirection.NorthWest), true)));
+        assertNotNull(modelFacade.buildSettlement(0, new BuildSettlement_Params(2, new VertexLocation(new HexLocation(0, 0), VertexDirection.West), true)));
+        assertNotNull(modelFacade.buildRoad(0, new BuildRoad_Params(1, new EdgeLocation(new HexLocation(0, 1), EdgeDirection.NorthEast), true)));
+        assertNotNull(modelFacade.buildSettlement(0, new BuildSettlement_Params(1, new VertexLocation(new HexLocation(0, 1), VertexDirection.NorthEast), true)));
+        assertNotNull(modelFacade.buildRoad(0, new BuildRoad_Params(0, new EdgeLocation(new HexLocation(-1, 2), EdgeDirection.North), true)));
+        assertNotNull(modelFacade.buildSettlement(0, new BuildSettlement_Params(0, new VertexLocation(new HexLocation(-1, 2), VertexDirection.NorthWest), true)));
+        
+        // Check that it's player[0]'s turn
+        assertTrue(modelFacade.getGame(0).getTurnTracker().getCurrentPlayerIndex() == 0);
+        
         // Test the correct player ending their turn
+        assertNotNull(modelFacade.rollNumber(0, new RollNumber_Params(0, 6)));
         assertNotNull(modelFacade.finishTurn(0, new FinishTurn_Params(0)));
+        
+        // Test someone ending their turn when it's not their turn currently
+        assertNotNull(modelFacade.rollNumber(0, new RollNumber_Params(1, 6)));
+        assertNull(modelFacade.finishTurn(0, new FinishTurn_Params(2)));
+        
+        // Progress through everyone's turn so it's back to player[0]
+        assertNotNull(modelFacade.finishTurn(0, new FinishTurn_Params(1)));
+        assertNotNull(modelFacade.rollNumber(0, new RollNumber_Params(2, 6)));
+        assertNotNull(modelFacade.finishTurn(0, new FinishTurn_Params(2)));
+        assertNotNull(modelFacade.rollNumber(0, new RollNumber_Params(3, 6)));
+        assertNotNull(modelFacade.finishTurn(0, new FinishTurn_Params(3)));
+        
+        // Check that it's player[0]'s turn again
+        assertTrue(modelFacade.getGame(0).getTurnTracker().getCurrentPlayerIndex() == 0);
 
         // After turn ends, the game state should be Rolling
         assertTrue(modelFacade.getGame(0).getTurnState() == ServerTurnState.Rolling);
