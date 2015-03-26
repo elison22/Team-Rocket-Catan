4a2e9fe15bfbc7455d9abfc5b0a86e698ef21859
Chad Bacon <Chadsbacon@gmail.com>
2015-03-25 19:29:31 -0600

    Wrote more extensive tests for longestRoad

--- java/server/test/command/LongestRoadTest.java
+++ java/server/test/command/LongestRoadTest.java
@@ -96,13 +96,13 @@ public class LongestRoadTest {
         assertNotNull(modelFacade.finishTurn(0, new FinishTurn_Params(0)));
         assertNotNull(modelFacade.rollNumber(0, new RollNumber_Params(1, 6)));
         
-        // Retrieve player[0]'s wood and bricks
+        // Retrieve player[0]'s bricks
         HashMap<ResourceType, Integer> tradeOffer = new HashMap<ResourceType, Integer>();
         tradeOffer.put(ResourceType.BRICK, -5);
         tradeOffer.put(ResourceType.ORE, 1);
         tradeOffer.put(ResourceType.SHEEP, 0);
         tradeOffer.put(ResourceType.WHEAT, 0);
-        tradeOffer.put(ResourceType.WOOD, -5);
+        tradeOffer.put(ResourceType.WOOD, 0);
         assertNotNull(modelFacade.offerTrade(0, new OfferTrade_Params(1, tradeOffer, 0)));
         assertNotNull(modelFacade.acceptTrade(0, new AcceptTrade_Params(0, true)));
         
@@ -112,11 +112,50 @@ public class LongestRoadTest {
         assertNotNull(modelFacade.buildRoad(0, new BuildRoad_Params(1, new EdgeLocation(new HexLocation(2, -2), EdgeDirection.NorthEast), false)));
         assertNotNull(modelFacade.buildRoad(0, new BuildRoad_Params(1, new EdgeLocation(new HexLocation(2, -2), EdgeDirection.North), false)));
         
-        // Check that the player has longest road
+        // Check that player[1] has longest road
         assertTrue(game.getTurnTracker().getLongestRoadPlayerIndex() == 1);
         
-        // Check that the player was rewarded victory points
+        // Check that player[1] was rewarded victory points
         assertTrue(game.getPlayerList().get(1).getVictoryPoints() == 4);
+        
+        // Roll a bunch of 8's to get 6 brick and 6 wood for player[0]
+        assertNotNull(modelFacade.finishTurn(0, new FinishTurn_Params(1)));
+        assertNotNull(modelFacade.rollNumber(0, new RollNumber_Params(2, 8)));
+        assertNotNull(modelFacade.finishTurn(0, new FinishTurn_Params(2)));
+        assertNotNull(modelFacade.rollNumber(0, new RollNumber_Params(3, 8)));
+        assertNotNull(modelFacade.finishTurn(0, new FinishTurn_Params(3)));
+        assertNotNull(modelFacade.rollNumber(0, new RollNumber_Params(0, 8)));
+        assertNotNull(modelFacade.finishTurn(0, new FinishTurn_Params(0)));
+        assertNotNull(modelFacade.rollNumber(0, new RollNumber_Params(1, 8)));
+        assertNotNull(modelFacade.finishTurn(0, new FinishTurn_Params(1)));
+        assertNotNull(modelFacade.rollNumber(0, new RollNumber_Params(2, 8)));
+        assertNotNull(modelFacade.finishTurn(0, new FinishTurn_Params(2)));
+        assertNotNull(modelFacade.rollNumber(0, new RollNumber_Params(3, 8)));
+        assertNotNull(modelFacade.finishTurn(0, new FinishTurn_Params(3)));
+        assertNotNull(modelFacade.rollNumber(0, new RollNumber_Params(0, 6)));
+        
+        // Build 4 roads to tie for longest road
+        assertNotNull(modelFacade.buildRoad(0, new BuildRoad_Params(0, new EdgeLocation(new HexLocation(-1, -1), EdgeDirection.SouthEast), false)));
+        assertNotNull(modelFacade.buildRoad(0, new BuildRoad_Params(0, new EdgeLocation(new HexLocation(-1, -1), EdgeDirection.NorthEast), false)));
+        assertNotNull(modelFacade.buildRoad(0, new BuildRoad_Params(0, new EdgeLocation(new HexLocation(-1, -1), EdgeDirection.North), false)));
+        assertNotNull(modelFacade.buildRoad(0, new BuildRoad_Params(0, new EdgeLocation(new HexLocation(-1, -1), EdgeDirection.NorthWest), false)));
+        
+        // Check that player[0] doesn't have longest road yet
+        assertFalse(game.getTurnTracker().getLongestRoadPlayerIndex() == 0);
+        
+        // Build last road to get longest road
+        assertNotNull(modelFacade.buildRoad(0, new BuildRoad_Params(0, new EdgeLocation(new HexLocation(-1, -1), EdgeDirection.SouthWest), false)));
+        
+        // Check that player[0] has longest road
+        assertTrue(game.getTurnTracker().getLongestRoadPlayerIndex() == 0);
+        
+        // Check that player[0] was rewarded victory points
+        assertTrue(game.getPlayerList().get(0).getVictoryPoints() == 4);
+        
+        // Check that player[1] lost the longest road points
+        assertTrue(game.getPlayerList().get(1).getVictoryPoints() == 2);
+        
+        
 	}
 
 }
