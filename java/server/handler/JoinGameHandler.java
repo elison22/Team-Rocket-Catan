37278a86c0df79ac5742bc9e5f454306f89c1378
Andrew <wally451@sbcglobal.net>
2015-03-25 22:10:41 -0600

    .

--- java/server/handler/JoinGameHandler.java
+++ java/server/handler/JoinGameHandler.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 
 import shared.dto.JoinGame_Params;
+import user.IUserFacade;
 
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
@@ -13,14 +14,17 @@ import facade.IModelFacade;
 public class JoinGameHandler extends NonMoveHandler {
 	
 	private IModelFacade modelFacade;
+	private IUserFacade userFacade;
 	
-	public JoinGameHandler(IModelFacade modelFacade) {
+	public JoinGameHandler(IModelFacade modelFacade, IUserFacade userFacade) {
 		super();
 		this.modelFacade = modelFacade;
+		this.userFacade = userFacade;
 	}
 
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
+System.out.println("1");
 		String[] cookie = decodeCookie(exchange);
 		StringBuilder stringBuild = handleRequestBody(exchange);
 		JoinGame_Params params = gson.fromJson(stringBuild.toString(), JoinGame_Params.class);
@@ -30,7 +34,14 @@ public class JoinGameHandler extends NonMoveHandler {
 		int gameId = modelFacade.getCreatedGameId();
 		String encode = "catan.game=" + gameId + ";Path=/;";
 		head.add("Set-cookie", encode);
-		
+/*System.out.println(cookie[0] + ", " + cookie[2]);
+		if(!userFacade.hasUser(cookie[0])) {
+System.out.println("bad");
+			exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, 0);
+			sendResponseBody(exchange, "User is not registered");
+			exchange.close();
+			return;
+		}*/
 		if(modelFacade.joinGame(params, cookie[0], new Integer(cookie[2]))) {
 			exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
 			sendResponseBody(exchange, "Success");
