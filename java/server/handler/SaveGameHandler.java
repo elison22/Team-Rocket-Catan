b2b4243412f0ba7cb8785bb99c8a5e13056ed052
Andrew <wally451@sbcglobal.net>
2015-03-30 19:10:09 -0600

    added check for another issue

--- java/server/handler/SaveGameHandler.java
+++ java/server/handler/SaveGameHandler.java
@@ -28,26 +28,18 @@ public class SaveGameHandler extends NonMoveHandler {
 	public void handle(HttpExchange exchange) throws IOException {
 		Headers head = null;
 		StringBuilder string = handleRequestBody(exchange);
-//System.out.println(string.toString());
-//		JsonObject js = new JsonParser().parse(string.toString()).getAsJsonObject();
-//System.out.println(js.has("name"));
-//System.out.println("here");
-//System.out.println(js);
-//		JsonElement gameId = js.get("id");
-//		JsonElement fileName = js.get("name");
-//System.out.println(gameId.getAsString() + ", " + fileName.getAsString());
-//		if(fileName.isJsonNull() || gameId.isJsonNull()) {
-//			head = exchange.getResponseHeaders();
-//			head.set("Content-Type", "text/html");
-//			head.add("Set-cookie", "catan.game=" + 0 + ";Path=/;");
-//			exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, 0);
-//			sendResponseBody(exchange, "Invalid request");
-//			exchange.close();
-//			return;
-//		}
 		
 		if(!string.toString().equals("")) {
 			String[] values = string.toString().split(",");
+			if(values.length != 2) {
+				head = exchange.getResponseHeaders();
+				head.set("Content-Type", "text/html");
+				head.add("Set-cookie", "catan.game=" + 0 + ";Path=/;");
+				exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, 0);
+				sendResponseBody(exchange, "Invalid request");
+				exchange.close();
+				return;
+			}
 			
 			String[] game = values[0].split(":");
 			String[] file = values[1].split(":");
@@ -76,17 +68,15 @@ public class SaveGameHandler extends NonMoveHandler {
 			head = exchange.getResponseHeaders();
 			head.set("Content-Type", "text/html");
 			head.add("Set-cookie", "catan.game=" + gameId + ";Path=/;");
-System.out.println("here");
+
 			if(modelFacade.saveGame(new Integer(gameId), fileName)) {
-				System.out.println("here1");
 				exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
 				sendResponseBody(exchange, "Success");
 			} else {
-				System.out.println("here2");
 				exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, 0);
 				sendResponseBody(exchange, "Could not save game");
 			}
-//System.out.println("here");
+
 		} else {
 			head = exchange.getResponseHeaders();
 			head.set("Content-Type", "text/html");
