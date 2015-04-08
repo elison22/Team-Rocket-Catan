2ad62b2613490d472e554e79d0ef15b7135e28f4
gseccles <gseccles@gmail.com>
2015-04-08 09:51:35 -0600

    Stubbed out methods for IUserDAO and IPersistanceManager and created javadocs

--- java/server/plugin/IPersistanceManager.java
+++ java/server/plugin/IPersistanceManager.java
@@ -1,5 +1,21 @@
 package plugin;
 
+/**
+ * This class manages access to the data access objects for the rest of the program
+ * @author gseccles
+ *
+ */
 public interface IPersistanceManager {
-
+	
+	/**
+	 * Provides access to the game data access object, which provides access to the database
+	 * @return Abstract game data access object (works for multiple types of databases)
+	 */
+	public IGameDAO getGameDAO();
+	
+	/**
+	 * Provides access to the user data access object, which provides access to the database
+	 * @return Abstract user data access object (works for multiple types of databases)
+	 */
+	public IUserDAO getUserDAO();
 }
--- java/server/plugin/IUserDAO.java
+++ java/server/plugin/IUserDAO.java
@@ -1,5 +1,43 @@
 package plugin;
 
-public interface IUserDAO {
+import user.model.User;
 
+/**
+ * This class defines all of the methods that will be called by the server
+ * to perform any user-related operation with any given database.
+ * @author gseccles
+ *
+ */
+public interface IUserDAO {
+	
+	/**
+	 * Adds a new user to the current database
+	 * @param userName
+	 * @param password
+	 * @return true if the operations succeeds, otherwise false
+	 */
+	public boolean addUser(String userName, String password);
+	
+	/**
+	 * Updates user information in the current database
+	 * @param password
+	 * @return true if the operations succeeds, otherwise false
+	 */
+	public boolean updateUser(String password);
+	
+	/**
+	 * Authenticates user login information
+	 * @param userName
+	 * @param password
+	 * @return true if the operations succeeds, otherwise false
+	 */
+	public boolean validateUser(String userName, String password);
+	
+	/**
+	 * Retrieves a user from the database based on its username and password
+	 * @param userName
+	 * @param password
+	 * @return User with the matching username and password
+	 */
+	public User getUser(String userName, String password);
 }
